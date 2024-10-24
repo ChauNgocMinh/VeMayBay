1687dbbe2f6af28c141fb999e834d49d823cc31d
minh.chau <minh.chau@bmbsoft.com.vn>
2024-10-24 17:47:38 +0700

    update

---
+++ BANVEMAYBAY/Global.asax.cs
@@ -0,0 +1,32 @@
+﻿using BANVEMAYBAY;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace BANVEMAYBAY
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+        protected void Session_Start()
+        {
+            Session["cart"] = "";
+            Session["favoriteProduct"] = "";
+            Session["Admin_id"] = "";
+            Session["Admin_user"] = "";
+            Session["Admin_fullname"] = "";
+            Session["userName11"] = "";
+            Session["Message"] = "";
+            Session["id"] = "";
+            Session["user"] = "";
+
+        }
+    }
+}
